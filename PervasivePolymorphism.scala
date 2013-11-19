d53437af55ab54ccf4113f7739e3f3e44dccba3e
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-11-19 18:13:05 +0100

    finish unification

--- PervasivePolymorphism.scala
+++ PervasivePolymorphism.scala
@@ -127,72 +127,6 @@ trait TypesAndTerms extends Terms with Types {
   }
 }
 
-trait Pretty extends TypesAndTerms {
-  trait PrettyVisitor extends Visitor[(String, Int)]
-  {
-    private type Domain = (String, Int)
-
-    override def ∀(name: Name, body: Domain) = body match {
-      case (body, pBody) =>
-        ("∀%s. %s".format(
-          name.toString,
-          paren(pBody, priority_∀ + 1, body)
-        ), priority_∀)
-    }
-
-    override def →(σ : Domain, τ : Domain) = (σ , τ) match {
-      case ((σ, priority_σ), (τ, priority_τ)) =>
-        ("%s → %s".format(
-          paren(priority_σ, priority_→,     σ),
-          paren(priority_τ, priority_→ + 1, τ)
-        ), priority_→)
-    }
-
-    override def α(name: Name) = (name.toString, priority_↓)
-    override def ℤ = ("ℤ", priority_↓)
-
-    def χ(name: Name): Domain = (name.toString, priority_↓)
-
-    override def λ(name: Name, body: Domain): Domain = body match {
-      case (body, pBody) =>
-        ("λ%s. %s".format(
-          name.toString,
-          paren(pBody, priority_λ + 1, body)
-        ), priority_λ)
-    }
-
-    override def ε(f: Domain, x: Domain) = (f, x) match {
-      case ((f, pf), (x, px)) =>
-        ("%s %s".format(
-          paren(pf, priority_ε + 1, f),
-          paren(px, priority_ε,     x)
-        ), priority_ε)
-    }
-
-    override def ϕ(value: Int) = (value.toString, priority_↓)
-    override def Σ = ("Σ", priority_↓)
-
-    val priority_↑ = 3 // outermost priority
-    val priority_λ = 2
-    val priority_∀ = 2
-    val priority_ε = 1
-    val priority_→ = 1
-    val priority_↓ = 0
-
-    def paren(innerPriority: Int, outerPriority: Int, text: String):
-        String =
-      if (innerPriority < outerPriority)
-        text
-      else
-        "(%s)" format text
-  }
-
-  object PrettyVisitor extends PrettyVisitor
-
-  def pretty(t : Term): String = PrettyVisitor(t)._1
-  def pretty(τ : Type): String = PrettyVisitor(τ)._1
-}
-
 trait Reconstruction extends TypesAndTerms {
   topLevel =>
 
@@ -440,252 +374,189 @@ trait TypedTerms extends TypesAndTerms {
   }
 }
 
+
+trait Pretty extends TypedTerms {
+  trait PrettyVisitor extends Visitor[(String, Int)]
+  {
+    private type Domain = (String, Int)
+
+    override def ∀(name: Name, body: Domain) = body match {
+      case (body, pBody) =>
+        ("∀%s. %s".format(
+          name.toString,
+          paren(pBody, priority_∀ + 1, body)
+        ), priority_∀)
+    }
+
+    override def →(σ : Domain, τ : Domain) = (σ , τ) match {
+      case ((σ, priority_σ), (τ, priority_τ)) =>
+        ("%s → %s".format(
+          paren(priority_σ, priority_→,     σ),
+          paren(priority_τ, priority_→ + 1, τ)
+        ), priority_→)
+    }
+
+    override def α(name: Name) = (name.toString, priority_↓)
+    override def ℤ = ("ℤ", priority_↓)
+
+    def χ(name: Name): Domain = (name.toString, priority_↓)
+
+    override def λ(name: Name, body: Domain): Domain = body match {
+      case (body, pBody) =>
+        ("λ%s. %s".format(
+          name.toString,
+          paren(pBody, priority_λ + 1, body)
+        ), priority_λ)
+    }
+
+    override def ε(f: Domain, x: Domain) = (f, x) match {
+      case ((f, pf), (x, px)) =>
+        ("%s %s".format(
+          paren(pf, priority_ε + 1, f),
+          paren(px, priority_ε,     x)
+        ), priority_ε)
+    }
+
+    override def ϕ(value: Int) = (value.toString, priority_↓)
+    override def Σ = ("Σ", priority_↓)
+
+    val priority_↑ = 3 // outermost priority
+    val priority_λ = 2
+    val priority_∀ = 2
+    val priority_ε = 1
+    val priority_→ = 1
+    val priority_↓ = 0
+
+    def paren(innerPriority: Int, outerPriority: Int, text: String):
+        String =
+      if (innerPriority < outerPriority)
+        text
+      else
+        "(%s)" format text
+  }
+
+  object PrettyVisitor extends PrettyVisitor
+
+  def pretty(t : Term): String = PrettyVisitor(t)._1
+  def pretty(τ : Type): String = PrettyVisitor(τ)._1
+  def pretty(t : TypedTerm): String =
+    "%s : %s".format(pretty(t.term), pretty(t.getType))
+}
+
 trait Unification extends Substitution with TypedTerms {
-  import collection.immutable.HashMap
-  type Context = HashMap[Name, Type]
-  val ∅ : Context = HashMap.empty
-  implicit def singletonToContext(p: (Name, Type)): Context = HashMap(p)
+  object Unification {
+    import collection.immutable.HashMap
+    type Context = HashMap[Name, Type]
+    val ∅ : Context = HashMap.empty
+    implicit def singletonToContext(p: (Name, Type)): Context = HashMap(p)
 
-  case class Judgement(Γ : Context, t : Term, τ : Type)
+    case class Judgement(Γ : Context, t : Term, τ : Type)
 
-  case class EqConstraint(lhs: Type, rhs: Type)
+    case class EqConstraint(lhs: Type, rhs: Type)
 
-  class U01_GatherConstraints
-  extends TermVisitor[(Judgement, List[EqConstraint], List[Type])] {
-    private[this] type T = (Judgement, List[EqConstraint], List[Type])
+    class U01_GatherConstraints
+    extends TermVisitor[(List[Judgement], List[EqConstraint])] {
+      private[this] type T = (List[Judgement], List[EqConstraint])
 
-    // could have used prealgebra composition with Reconstruction
-    private[this] object R extends TermReconstruction
+      // could have used prealgebra composition with Reconstruction
+      private[this] object R extends TermReconstruction
 
-    def χ(name: Name): T = {
-      val τ = α(getGenerativeName)
-      (Judgement(name -> τ, R.χ(name), τ), Nil, Nil)
-    }
+      def χ(name: Name): T = {
+        val τ = α(getGenerativeName)
+        (Judgement(name -> τ, R.χ(name), τ) :: Nil, Nil)
+      }
 
-    def λ(name: Name, body: T): T = {
-      val (Judgement(_Γ, t, τ), constraints, typeArgs) = body
-      val σ = _Γ.applyOrElse[Name, Type](name, _ => α(getGenerativeName))
-      (Judgement(_Γ - name, R.λ(name, t), σ →: τ), constraints,
-        σ :: typeArgs)
-    }
+      def λ(name: Name, body: T): T = {
+        val (Judgement(_Γ, t, τ) :: _, constraints) = body
+        val σ = _Γ.applyOrElse[Name, Type](name, _ => α(getGenerativeName))
+        (Judgement(_Γ - name, R.λ(name, t), σ →: τ) :: body._1, constraints)
+      }
 
-    def ε(operator: T, operand: T): T = {
-      val (Judgement(f_Γ, f, f_τ), f_constraints, f_typeArgs) = operator
-      val (Judgement(x_Γ, x, σ  ), x_constraints, x_typeArgs) = operand
-      val τ = α(getGenerativeName)
-      val Γ = (f_Γ merged x_Γ) { case ((name, τ1), _) => (name, τ1) }
-      val constraints = EqConstraint(f_τ, σ →: τ) :: (
-        f_constraints ++ x_constraints ++
-          f_Γ.filter(x_Γ contains _._1).map({
-            case (name, τ1) => EqConstraint(τ1, x_Γ(name))
-          })
-      )
-      (Judgement(Γ, R.ε(f, x), τ), constraints,
+      def ε(operator: T, operand: T): T = {
+        val (Judgement(f_Γ, f, f_τ) :: _, f_constraints) = operator
+        val (Judgement(x_Γ, x, σ  ) :: _, x_constraints) = operand
+        val τ = α(getGenerativeName)
+        val Γ = (f_Γ merged x_Γ) { case ((name, τ1), _) => (name, τ1) }
+        val constraints = EqConstraint(f_τ, σ →: τ) :: (
+          f_constraints ++ x_constraints ++
+            f_Γ.filter(x_Γ contains _._1).map({
+              case (name, τ1) => EqConstraint(τ1, x_Γ(name))
+            })
+        )
         // Argument order of ++ is very important!
-        // Since hidden type arguments are prepended in evaluation order,
+        // Since hidden type judgements are prepended in evaluation order,
         // the subterm f that's evaluated first contributes to a later
         // part of the list of hidden arguments.
-        x_typeArgs ++ f_typeArgs)
+        ( Judgement(Γ, R.ε(f, x), τ) :: (operand._1 ++ operator._1),
+          constraints )
+      }
+
+      def ϕ(value: Int): T = (Judgement(∅, R.ϕ(value), ℤ) :: Nil, Nil)
+      def Σ : T = (Judgement(∅, R.Σ, ℤ →: ℤ →: ℤ) :: Nil, Nil)
     }
 
-    def ϕ(value: Int): T = (Judgement(∅, R.ϕ(value), ℤ), Nil, Nil)
-    def Σ : T = (Judgement(∅, R.Σ, ℤ →: ℤ →: ℤ), Nil, Nil)
-  }
+    def U02_MGS(constraints: List[EqConstraint]):
+        Option[Map[Name, Type]] = {
+      type Eq = EqConstraint
+      val  Eq = EqConstraint
+      constraints match {
+        case Nil =>
+          Some(Map.empty)
 
-  def U02_MGS(constraints: List[EqConstraint]):
-      Option[Map[Name, Type]] = {
-    type Eq = EqConstraint
-    val  Eq = EqConstraint
-    constraints match {
-      case Nil =>
-        Some(Map.empty)
+        case Eq(σ1 → τ1, σ2 → τ2) :: others =>
+          U02_MGS(Eq(σ1, σ2) :: Eq(τ1, τ2) :: others)
 
-      case Eq(σ1 → τ1, σ2 → τ2) :: others =>
-        U02_MGS(Eq(σ1, σ2) :: Eq(τ1, τ2) :: others)
+        case Eq(α(name), τ) :: others =>
+          U02_MGS(others map { case Eq(τ1, τ2) =>
+            Eq(τ1 substitute (name -> τ),
+              τ2 substitute (name -> τ))
+          }) map { mgs => mgs.updated(name, τ substitute mgs) }
 
-      case Eq(α(name), τ) :: others =>
-        U02_MGS(others map { case Eq(τ1, τ2) =>
-          Eq(τ1 substitute (name -> τ),
-             τ2 substitute (name -> τ))
-        }) map { mgs => mgs.updated(name, τ substitute mgs) }
+        case Eq(τ, α(name)) :: others =>
+          U02_MGS(Eq(α(name), τ) :: others)
 
-      case Eq(τ, α(name)) :: others =>
-        U02_MGS(Eq(α(name), τ) :: others)
-
-      case _ =>
-        None
-    }
-  }
-
-  class U03_Inference(
-    mostGeneralSubstitution: Map[Name, Type],
-    judgements: List[Judgement]
-  )
-  extends TermVisitor[TypedTerm]
-  {
-    val mgs = mostGeneralSubstitution.orElse[Name, Type] {
-      case name => α(name)
+        case Eq(τ1, τ2) :: others =>
+          if (τ1 == τ2) U02_MGS(others) else None
+      }
     }
 
-    private[this] type T = TypedTerm
-    def χ(name: Name): T = ???
-    def λ(name: Name, body: T): T = ???
-    def ε(operator: T, operand: T): T = ???
+    class U03_Inference(mgs: Map[Name, Type], judgements: List[Judgement])
+    extends TermVisitor[TypedTerm]
+    {
+      private[this] type T = TypedTerm
 
-    def ϕ(value: Int): T = ???
-    def Σ : T = ???
+      val jStack = collection.mutable.Stack(judgements.reverse: _*)
+
+      private[this] def default: T = jStack.pop match {
+        case Judgement(_, t, τ) =>
+          TypedTerm(t, τ substitute mgs)
+      }
+
+      def λ(name: Name, body: T): T = jStack.pop match {
+        case Judgement(_, t, τ) =>
+          TypedTerm(t, τ substitute mgs, body)
+      }
+
+      def ε(operator: T, operand: T): T = jStack.pop match {
+        case Judgement(_, t, τ) =>
+          TypedTerm(t, τ substitute mgs, operator, operand)
+      }
+
+      def χ(name: Name): T = default
+      def ϕ(value: Int): T = default
+      def Σ : T            = default
+    }
+
+    def infer(t: Term): Option[TypedTerm] = {
+      val(judgements, constraints) = (new U01_GatherConstraints)(t)
+      U02_MGS(constraints) map { mgs =>
+        new U03_Inference(mgs, judgements)(t)
+      }
+    }
   }
 }
 
-/*
-trait Unification extends TypesAndTerms with UntypedTerms {
-  case class Unify(lhs: Type, rhs: Type) {
-    override def toString: String = s"$lhs == $rhs"
-  }
-
-  // Run into expression problem in the language of types.
-  // TODO take other type constructors into consideration
-  // in ₋freeTypeVars₋ and ₋tsub₋... or just use ₋Product₋
-  // and ₋isInstanceOf₋.
-
-  def freeTypeVars(tau: Type): Set[TypeVar] = tau match {
-    case x @ TypeVar(_) =>
-      Set(x)
-
-    case ∀(boundTypeVar, body) =>
-      freeTypeVars(body) - boundTypeVar
-
-    case σ →: τ =>
-      freeTypeVars(σ) ++ freeTypeVars(τ)
-
-    case _ =>
-      Set.empty
-  }
-  implicit class typeVarDotIsFreeIn(x: TypeVar) {
-    def isFreeIn(tau: Type): Boolean =
-      freeTypeVars(tau) contains x
-  }
-
-  def tsubAll(subs: Map[TypeVar, Type], tau: Type): Type =
-    subs.foldRight(tau) { (keyValuePair, wipType) =>
-      val (x, σ) = keyValuePair
-      tsub(x, σ, wipType)
-    }
-
-  // capture avoidance...
-  def tsub(x: TypeVar, tau: Type, body: Type): Type = body match {
-    case y @ TypeVar(_) if x == y =>
-      tau
-
-    // premature optimization
-    case quantifiedType @ ∀(boundTypeVar, body)
-        if tau.isInstanceOf[TypeVar] =>
-      if (tau == boundTypeVar)
-        quantifiedType
-      else
-        ∀(boundTypeVar, tsub(x, tau, body))
-
-    case ∀(boundTypeVar, body) =>
-      if (boundTypeVar isFreeIn tau) {
-        val newBoundTypeVar = TypeVar(getFreshName(boundTypeVar.name))
-        ∀(newBoundTypeVar,
-          tsub(x, tau,
-            tsub(boundTypeVar, newBoundTypeVar, body)))
-      }
-      else
-        ∀(boundTypeVar, tsub(x, tau, body))
-
-    case σ →: τ =>
-      tsub(x, tau, σ) →: tsub(x, tau, τ)
-
-    case _ => body
-  }
-
-  type Constraints = List[Unify]
-  type TypingContext = Map[Name, Type]
-
-  def getUnificationConstraints(t: UntypedTerm):
-      (Type, Term, Constraints) = {
-    // ∅ is an operator symbol, not a letter, thus the space before colon.
-    val ∅ : TypingContext = Map.empty
-    def loop(t: UntypedTerm, Γ: TypingContext):
-        (Type, Term, Constraints) =
-      t match {
-        case ForgetfulTerm(t) =>
-          (t.getType, t, Nil)
-
-        case UntypedVar(x) =>
-          (Γ(x), Var(x, Γ(x)), Nil)
-
-        case λ(x, body) =>
-          val σ = TypeVar(getFreshName("σ"))
-          val (τ, typedBody, constraints) = loop(body, Γ.updated(x, σ))
-          (σ →: τ, Abs(Var(x, σ), typedBody), constraints)
-
-        case operator ! operand =>
-          val τ = TypeVar(getFreshName("τ"))
-          val (operatorType, typedOperator, acc0) =
-            loop(operator, Γ)
-          val (operandType, typedOperand, acc1) =
-            loop(operand, Γ)
-          ( τ,
-            App(typedOperator, typedOperand),
-            Unify(operatorType, operandType →: τ) :: (acc0 ++ acc1) )
-      }
-    loop(t, ∅)
-  }
-
-  def getMostGeneralSubstitutions(constraints: Constraints):
-      Map[TypeVar, Type] = constraints match {
-    case Nil =>
-      Map.empty
-
-    case (unify @ Unify(σ, τ)) :: constraints
-        if σ.isInstanceOf[TypeVar] || τ.isInstanceOf[TypeVar] =>
-      val (x, body) = (σ, τ) match {
-        case (x @ TypeVar(_), _) => (x, τ)
-        case (_, y @ TypeVar(_)) => (y, σ)
-      }
-      if (x isFreeIn body)
-        sys error s"Type variable $x appears on both sides of $unify"
-      else {
-        val mgss = getMostGeneralSubstitutions(constraints map {
-          case Unify(a, b) =>
-            Unify(tsub(x, body, a), tsub(x, body, b))
-        })
-        val xType = tsubAll(mgss, body)
-        mgss.updated(x, xType)
-      }
-
-    case Unify(a →: b, c →: d) :: constraints =>
-      getMostGeneralSubstitutions(Unify(a, c) :: Unify(b, d) :: constraints)
-
-    case Unify(a, b) :: constraints if a == b =>
-      getMostGeneralSubstitutions(constraints)
-  }
-
-  def inferSimpleType(t0: UntypedTerm): Term = {
-    val (τ, t, constraints) = getUnificationConstraints(t0)
-    val mgss = getMostGeneralSubstitutions(constraints)
-    def useMGSS(τ: Type): Type = tsubAll(mgss, τ)
-    def applyMGSS(t: Term): Term = t match {
-      case Var(x, τ) =>
-        Var(x, useMGSS(τ))
-
-      case Abs(Var(x, τ), body) =>
-        Abs(Var(x, useMGSS(τ)), applyMGSS(body))
-
-      case App(operator, operand) =>
-        App(applyMGSS(operator), applyMGSS(operand))
-
-      case _ =>
-        t
-    }
-    applyMGSS(t)
-  }
-}
-
-trait MinimallyQuantifiedTypes extends QuantifiedTypes {
+trait MinimallyQuantifiedTypes extends Types {
   // A quantified type is minimally quantified if every quantifier
   // attaches to the least common ancestor of all occurrences of
   // the type variable it binds.
@@ -704,14 +575,8 @@ trait MinimallyQuantifiedTypes extends QuantifiedTypes {
   case object Meh extends Type
 }
 
-trait WeirdCalculus extends UntypedTerms with MinimallyQuantifiedTypes {
-}
-
- */
-
 object TestEverything
-//extends WeirdCalculus with Unification {
-extends Pretty with Substitution {
+extends Pretty with Unification {
   def main(args: Array[String]) {
     val hole1 = Hole.spawn(1).head
     val hole2 = Hole.spawn(1).head
@@ -724,11 +589,7 @@ extends Pretty with Substitution {
     val τ = (σ << Map(hole2 -> ℤ)) renameAll
       Map("r" -> "α") substitute
       ("α" -> "β", "a" -> "α")
-    println(pretty(t))
     println(pretty(τ))
-    //val term = inferSimpleType(untypedTerm)
-    //println(s"Unification works!")
-    //println(s"type = ${term.getType}")
-    //println(s"term = $term")
+    println(pretty(Unification.infer(t).get))
   }
 }
