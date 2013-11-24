277c2652f2afdbeae6da2175aa5e8709444e19e6
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-11-24 13:21:34 +0100

    parse lambda abstractions

--- Parser.scala
+++ Parser.scala
@@ -26,8 +26,7 @@ trait Parsing extends SystemMF with Pretty with RegexParsers {
     val colon  = "[:]".r
     val arrow  = "[→]".r
     val nil    = "".r
-    val ident  = """[^()\[\]{}",'`;#|\\∀λ→ \r\t\f\n]+""".r
-    val param  = """[^()\[\]{}",'`;#|\\∀λ→ \r\t\f\n.]+""".r
+    val ident  = """[^()\[\]{}",'`;#|\\∀λ→ \r\t\f\n.]+""".r
 
     // TYPE PARSERS
 
@@ -41,24 +40,25 @@ trait Parsing extends SystemMF with Pretty with RegexParsers {
     lazy val forallType: Parser[Type] =
       // ~> <~ cause problems here, no idea why
       forall ~ typeParamList ~ dot ~ typeExpr ^^ {
-        case _ ~ paramList ~ _ ~ bodyType => ∀(paramList: _*)(bodyType)
+        case _ ~ paramList ~ _ ~ body => ∀(paramList: _*)(body)
       }
 
     lazy val typeParamList: Parser[List[String]] =
-      opList(nil, param ^^ identity[String])
+      opList(nil, ident ^^ identity[String]) |
+                 (ident ^^ {x => List(x: String)})
 
     lazy val funType: Parser[Type] = opList(arrow, belowFun) ^^ {
-      _.foldRight[Type](ZeroInfo) { (domain, range) => range match {
-        case ZeroInfo  => domain
-        case otherwise => domain →: range
-      }}
+      _.foldRight[Type](ZeroInfo) { (domain, range) =>
+        if (ZeroInfo == range) domain
+        else                   domain →: range
+      }
     }
 
     lazy val typeApp: Parser[Type] = opList(nil, belowTypeApp) ^^ {
-      _.foldLeft[Type](ZeroInfo) { (typeFun, typeArg) => typeFun match {
-        case ZeroInfo  => typeArg
-        case otherwise => ★(typeFun, typeArg)
-      }}
+      _.foldLeft[Type](ZeroInfo) { (typeFun, typeArg) =>
+        if (ZeroInfo == typeFun) typeArg
+        else                     ★(typeFun, typeArg)
+      }
     }
 
     lazy val typeVar: Parser[Type] = ident ^^ { s => α(s) }
@@ -67,31 +67,52 @@ trait Parsing extends SystemMF with Pretty with RegexParsers {
 
     // TERM PARSERS
 
-    lazy val topLevel: Parser[SMFTerm] = termExpr ^^ {
-      case SMF(t, _Γ, names) => SMFTerm(t, _Γ, names)
-    }
-
     lazy val termExpr: Parser[SMF] = abs      | belowAbs
     lazy val belowAbs: Parser[SMF] = app      | belowApp
     lazy val belowApp: Parser[SMF] = variable | parenTerm
 
-    lazy val abs: Parser[SMF] = stupidParser
+    lazy val abs: Parser[SMF] =
+      lambda ~ termParam ~ dot ~ termExpr ^^ {
+        case _ ~ ((pName, pType)) ~ _ ~ body =>
+          val id = name.next
+          SMF(
+            λ(id, body.t rename (pName -> χ(id))),
+            body.Γ.updated(id, pType),
+            body.n.updated(id, pName)
+          )
+      }
 
-    lazy val app: Parser[SMF] = stupidParser
+    lazy val termParam: Parser[(String, Type)] =
+      ident ~ colon ~ typeExpr ^^ {
+        case pName ~ _ ~ pType => (pName, pType)
+      }
+
+    lazy val app: Parser[SMF] = opList(nil, belowApp) ^^ {
+      _.foldLeft[SMF](ZeroInfo) { (f, x) =>
+        if (ZeroInfo == f)
+          x
+        else {
+          assert((f.Γ.keySet & x.Γ.keySet).isEmpty)
+          assert((f.n.keySet & x.n.keySet).isEmpty)
+          SMF(ε(f.t, x.t), f.Γ ++ x.Γ, f.n ++ x.n)
+        }
+      }
+    }
 
     lazy val variable: Parser[SMF] = ident ^^ { s => SMF(χ(s), ∅, ∅) }
 
     lazy val parenTerm: Parser[SMF] = paren(termExpr)
 
-    lazy val stupidParser: Parser[SMF] =
-      "there's a pain that can't be spoken".r ^^ {
-        _ => sys error "there's a grief that goes on and on"
-      }
-
     // INTERNAL STUFF
 
+    lazy val topLevel: Parser[SMFTerm] = termExpr ^^ {
+      case SMF(t, _Γ, names) => SMFTerm(t, _Γ, names)
+    }
+
     // finite version of SMFTerm
-    case class SMF(t : Term, Γ : Map[Name, Type], nameScheme: Map[Name, Name])
+    case class SMF(t : Term, Γ : Map[Name, Type], n: Map[Name, Name])
+    object ZeroInfo extends SMF(ZeroInfoTerm, ∅, ∅) with Type
+    case object ZeroInfoTerm extends Term
 
     val name = new GenerativeNameGenerator
 
@@ -107,8 +128,6 @@ trait Parsing extends SystemMF with Pretty with RegexParsers {
       }
 
     def ∅[K, V] = Map.empty[K, V]
-
-    case object ZeroInfo extends Type
 
     def opList[T](operator: Regex, operand: Parser[T]): Parser[List[T]] =
       operand ~ (operator ~> operand).+ ^^ {
@@ -131,16 +150,23 @@ object TestParser extends Parsing {
   def t(s : String) {
     val t = parse(s).get
     println(t)
-    println(pretty(t.t))
+    println(pretty(t.getTerm))
     println
   }
 
+  val Y = """|λ f : (α → α) → (α → α) .
+             |  (λ x : ∀ β . β → α → α . f (x x))
+             |  (λ x : ∀ β . β → α → α . f (x x))
+             |""".stripMargin
+
   def main(args: Array[String]) {
     τ("α")
-    τ("α → β → γ")
-    τ("∀α β γ. α → (β → γ) → ((δ → ε) → η) → ζ")
+    τ("∀ α . α → β → γ")
+    τ("∀ α β γ . α → (β → γ) → ((δ → ε) → η) → ζ")
     τ("∀α β. (α → β) → List α → List β")
 
-    t("x")
+    t("λ f : α → β → γ . f (g x) (h y)")
+    t("λ x : ∀ β . β → α → α . f (x x)")
+    t(Y)
   }
 }
