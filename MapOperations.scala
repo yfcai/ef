3f8d021d1f89748560745c93a5c997e73efb8317
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-11-26 11:43:34 +0100

    optimize mapOp "restrict"

--- MapOperations.scala
+++ MapOperations.scala
@@ -4,9 +4,12 @@ trait MapOperations {
   implicit class ConvenientMapOpsForSMF[K, V](m: Map[K, V]) {
     def inverse: Map[V, K] = m map (_.swap)
 
-    def restrict(subdomain: Iterable[K]): Map[K, V] =
-      subdomain.flatMap({
-        k => if (m contains k) Some(k -> m(k)) else None
-      })(collection.breakOut)
+    def restrict(subdomain: Set[K]): Map[K, V] =
+      if (subdomain.size < m.size)
+        subdomain.flatMap({
+          k => if (m contains k) Some(k -> m(k)) else None
+        })(collection.breakOut)
+      else
+        m filter (subdomain contains _._1)
   }
 }
