83c16f2ac7a0e25a643926f82a9225dc95323992
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-11-27 14:45:54 +0100

    add all examples in Pierce 23.4 Warm-ups

--- F.scala
+++ F.scala
@@ -1,7 +1,13 @@
+import scala.language.implicitConversions
+
 trait SystemF extends TypedTerms with Substitution {
   object SystemF {
     case class Λ(alpha: Name, body: Term) extends Term
     case class □(term: Term, typeArg: Type) extends Term
+  }
+
+  implicit class typeApplicationForTerms[T <% Term](t: T) {
+    def □ (τ : Type): Term = SystemF.□(t, τ)
   }
 
   case class FTerm(canon: Term,
@@ -15,15 +21,6 @@ trait SystemF extends TypedTerms with Substitution {
       new FTermGlobalRenaming(names)(canon)
   }
 
-  object FTerm {
-    def apply[T <% Type](canon: Term, argTypes: (String, T)*): FTerm =
-      FTerm(canon,
-            argTypes.map({
-              case (s, τ) => (StringLiteral(s), τ : Type)
-            })(collection.breakOut): Map[Name, Type],
-            Map.empty[Name, Name])
-  }
-
   trait FTermVisitor[T] extends TermVisitor[T] {
     def χ(name: Name): T
     def λ(name: Name, body: T): T
@@ -35,6 +32,18 @@ trait SystemF extends TypedTerms with Substitution {
       case SystemF.Λ(alpha, body)   => Λ(alpha, apply(body))
       case SystemF.□(term, typeArg) => □(apply(term), typeArg)
       case _                        => super.apply(t)
+    }
+  }
+
+  implicit class EraseTypeAbstractionsAndApplications(t: Term) {
+    def erase: Term = (new ErasureVisitor)(t)
+
+    class ErasureVisitor
+    extends FTermVisitor[Term] with TermReconstruction
+    {
+      private[this] type T = Term
+      def Λ(alpha: Name, body: T): T = body
+      def □(term: T, typeArg: Type): T = term
     }
   }
 
@@ -104,15 +113,84 @@ trait PrettyF extends SystemF with Pretty {
 /** Examples of system F from Pierce's Types and Programming Languages */
 trait SystemFExamples extends SystemF {
   import SystemF._
-  val id = FTerm(Λ("α", λ("x")("x")), "x" -> "α")
+
+  /** An example of implementing primitives and literals by an infinite
+    * global environment
+    */
+  val Γ0: PartialFunction[Name, Type] =
+    Map(("succ": Name) -> "ℕ" →: "ℕ").orElse[Name, Type] {
+      // natural literals
+      case StringLiteral(s) if s matches """\d+""" =>
+        α("ℕ")
+    }
+
+  private[this]
+  def fTerm[T <% Type](canon: Term, localTypes: (String, T)*): FTerm =
+    fTerm(canon, localTypes.to_Γ)
+
+  private[this]
+  def fTerm(canon: Term, localTypes: PartialFunction[Name, Type]): FTerm = {
+    FTerm(canon, localTypes orElse Γ0, Map.empty[Name, Name])
+  }
+
+  private[this]
+  implicit class SequenceToContext[T <% Type](seq: Seq[(String, T)]) {
+    def to_Γ : Map[Name, Type] =
+      seq.map({
+        case (s, τ) => (StringLiteral(s), τ : Type)
+      })(collection.breakOut)
+  }
+
+  private[this]
+  implicit class ExtendPartialFunction
+    (f: PartialFunction[Name, Type])
+  {
+    def extend(g: PartialFunction[Name, Type]):
+        PartialFunction[Name, Type] = g orElse f
+
+    def extend[T <% Type](seq: (String, T)*):
+        PartialFunction[Name, Type] = extend(seq.to_Γ)
+  }
+
+  val id = fTerm(Λ("α", λ("x")("x")), "x" -> "α")
+
+  val idNat = fTerm(id.canon □ "ℕ", "x" -> "α")
+
+  val double = fTerm(
+    Λ("α", λ("f", "a")("f" ₋ ("f" ₋ "a"))),
+    "f" -> "α" →: "α",
+    "a" -> α("α")
+  )
+
+  val doubleNat = fTerm(double.canon □ "ℕ", double.Γ)
+
+  val doubleNatArrowNat = fTerm(double.canon □ ("ℕ" →: "ℕ"), double.Γ)
+
+  val quadrupleSucc3 = fTerm(
+    doubleNat.canon ₋ λ("x")("succ" ₋ ("succ" ₋ "x")) ₋ "3",
+    double.Γ.extend("x" -> "ℕ")
+  )
+
+  val selfApp = fTerm(
+    λ("x")("x" □ (id.getType) ₋ "x"),
+    "x" -> id.getType
+  )
+
+  val quadruple = fTerm(
+    Λ("α", double.canon □ ("α" →: "α") ₋ (double.canon □ "α")),
+    double.Γ
+  )
+
+  val listOfSystemFExamples = List(
+    id, idNat, double, doubleNat, doubleNatArrowNat,
+    quadrupleSucc3, selfApp, quadruple
+  )
 }
 
 object TestF extends SystemFExamples with PrettyF {
-  def p(t: FTerm) {
-    println(pretty(t))
-  }
-
   def main(args: Array[String]) {
-    p(id)
+    listOfSystemFExamples foreach {
+      t => println(pretty(t))
+    }
   }
 }
