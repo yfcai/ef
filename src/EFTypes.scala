a03290e5a32b5b86fd03a1db6b82f678a330d8e6
Cai Yufei <cai@mathematik.uni-marburg.de>
2013-12-19 22:11:47 +0100

    classify most general substitutions

--- src/EFTypes.scala
+++ src/EFTypes.scala
@@ -49,6 +49,109 @@ trait Unification extends Syntax with PrenexForm {
     }
     loop(constraints.toList) map { case (binder, τ) => (α(binder), τ) }
   }
+
+  // given mgs and two prenexes (for quantifiers only),
+  // produce:
+  //
+  //     (universal survivors, existential survivors, modified mgs)
+  //
+  def classifyMGS(mgs: Map[α, Type], lhs: PrenexForm, rhs: PrenexForm):
+      (Set[α], Set[α], Map[α, Type]) = {
+    import UnificationHelpers._
+    val all = collection.mutable.Set.empty[α] ++ lhs.all ++ rhs.all
+    val ex  = collection.mutable.Set.empty[α] ++ lhs.ex  ++ rhs.ex
+    val either = all ++ ex
+    def err(msg: String) = sys error (
+      "\n$msg in:\nfun : ${lhs.toType.unparse}" +
+                "\narg : ${rhs.toType.unparse}"
+    )
+    val modifiedMGS: Map[α, Type] = mgs map { case (a, τ) =>
+      all -= a
+      τ match {
+        // universal unified to universal, do nothing, or:
+        // universal unified to existential, do nothing beyond
+        // the deletion of the universal, which is already done
+        case b: α if either contains b =>
+          (a, b)
+
+        // unified to something not bound here, freak out
+        case b: α =>
+          err(s"$a unified to out-of-scope name $b")
+
+        // universal unified to a nontrivial type, tons of work
+        case τ0 =>
+          val τ = (ex & unbound(τ0)).foldRight(τ0) {
+            case (e, τ) =>
+              // any existential in a nontrivial type unified to
+              // a universal cannot appear anywhere outside.
+              ex -= e
+              // make sure that e appears nowhere else
+              if (count(e, τ0) != count(e, lhs.τ, rhs.τ))
+                err(s"the existential $e in ${τ0.unparse} escaped!")
+              // compute depth (number of greatest nesting to the
+              // left of function arrows)
+              val depthInside  = depth(e, τ0)
+              val depthOutside = depth(e, lhs.τ, rhs.τ)
+              assert(depthInside >= 0 && depthOutside >= 0)
+              // if depth parities are equal inside & out,
+              // quantify e existentially; otherwise universally
+              if (depthInside % 2 == depthOutside % 2)
+                ∃(e.binder.name) { x => τ subst (e, x) }
+              else
+                ∀(e.binder.name) { x => τ subst (e, x) }
+          }
+          (a, τ)
+      }
+    }
+    (all.toSet, ex.toSet, modifiedMGS)
+  }
+
+  object UnificationHelpers {
+    def unbound(τ: Type): Set[α] = {
+      import Type._
+      τ.fold[Set[α]] {
+        case α_(a)          => Set(α(a))
+        case b: Π1Binder[_] => b.body - α(b.binder)
+        case p: Π2[_]       => p.π1 ++ p.π2
+        case _              => Set.empty
+      }
+    }
+
+    def count(a: α, τ1: Type, τ2: Type): Int =
+      count(a, τ1) + count(a, τ2)
+
+    def count(a: α, τ: Type): Int = {
+      import Type._
+      τ.fold[Int] {
+        case α_(b) if α(b) == a => 1
+        case b: Π1[_]           => b.π1
+        case p: Π2[_]           => p.π1 + p.π2
+        case _: Π0[_]           => 0
+      }
+    }
+
+    // depth: the biggest number of arrows to the right of
+    // this variable
+    def depth(a: α, τ1: Type, τ2: Type): Int =
+      Math.max(depth(a, τ1), depth(a, τ2))
+
+    def depth(a: α, τ: Type): Int = {
+      import Type._
+      τ.fold[Int] {
+        case α_(b) if α(b) == a => 0
+        case →:(lhs, rhs)       => Math.max(1 + lhs, rhs)
+        case p: Π2[_]           => Math.max(p.π1, p.π2)
+        case b: Π1[_]           => b.π1
+        case _: Π0[_]           => Int.MinValue // I mean -∞
+      }
+    }
+
+    // how soon does a appear in τ
+    def ordinal(a: α, τ: Type): Int = {
+      val i = τ.traverse.indexOf(a)
+      if (i < 0) Int.MaxValue /* I mean ∞ */ else i
+    }
+  }
 }
 
 trait PrenexForm extends Types {
